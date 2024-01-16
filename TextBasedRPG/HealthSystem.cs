72c15a2b51bf592db31fa2c358442e08189fc8b8
Dylan <dylanryan431@gmail.com>
2024-01-16 09:00:03 -0400

    Set up basic classes
    Player,Enemy,Map,HealthSystem

---
+++ TextBasedRPG/HealthSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPG
+{
+    internal class HealthSystem
+    {
+        public int health;
+
+        //constructor
+        public HealthSystem(int health)
+        {
+            this.health = health;
+
+        }
+        public void TakeDamage(int hp)
+        {
+            health -= hp;
+        }
+
+        public void Heal(int hp)
+        {
+            health += hp;
+        }
+    }
+}
---
+++ TextBasedRPG/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPG
+{
+    internal class Player
+    {
+        public int x;
+        public int y;
+        public HealthSystem healthSystem;
+        public Player()
+        {
+            healthSystem = new HealthSystem(100);
+        }
+    }
+}
