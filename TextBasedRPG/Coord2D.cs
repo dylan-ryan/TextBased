94d394d842b538529cc217cbd55bf334e8e43a9e
Dylan <dylanryan431@gmail.com>
2024-01-17 14:52:34 -0400

    commit

---
+++ TextBasedRPG/Coord2D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPG
+{
+    internal struct Coord2D
+    {
+        public int x;
+        public int y;
+
+    }
+}
